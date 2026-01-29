4a802bdc91cf2e2caec7e50bba12036c7bd3af05
Wanja-Kayago <160125141+Wanja-Kayago@users.noreply.github.com>
2026-01-30 01:06:18 +0300

    fixed paths

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,45 +1,45 @@
 // ==================== PRODUCT DATA ====================
 const products = [
     // HOODIES
-    { id: 1, name: 'Classic White Hoodie', category: 'hoodies', image: 'assets/hoodies/hoodie1.jpeg', featured: true },
-    { id: 2, name: 'Sleeved Hoodie', category: 'hoodies', image: 'assets/hoodies/hoodie2.jpeg', featured: false },
-    { id: 3, name: 'White Pullover Hoodie', category: 'hoodies', image: 'assets/hoodies/hoodie3.jpeg', featured: false },
-    { id: 4, name: 'Pullover Hoodie', category: 'hoodies', image: 'assets/hoodies/hoodie4.jpeg', featured: false },
-    { id: 5, name: 'Orange-sleeved Pullover Hoodie', category: 'hoodies', image: 'assets/hoodies/hoodie5.jpeg', featured: false },
-    { id: 7, name: 'Gray Pullover Hoodie', category: 'hoodies', image: 'assets/hoodies/hoodie7.jpeg', featured: false },
-    { id: 8, name: 'Mint On Black Hoodie', category: 'hoodies', image: 'assets/hoodies/hoodie17.jpeg', featured: false },
-    { id: 9, name: 'Orange-sleeved Hoodie', category: 'hoodies', image: 'assets/hoodies/hoodie9.jpeg', featured: false },
-    { id: 10, name: 'Multi-designed Pullover Hoodie', category: 'hoodies', image: 'assets/hoodies/hoodie10.jpeg', featured: false },
-    { id: 11, name: 'Blue On Black Hoodie', category: 'hoodies', image: 'assets/hoodies/hoodie11.jpeg', featured: false },
-    { id: 12, name: 'Green On Black Hoodie', category: 'hoodies', image: 'assets/hoodies/hoodie16.jpeg', featured: false },
-    { id: 13, name: 'Orange On Black Hoodie', category: 'hoodies', image: 'assets/hoodies/hoodie13.jpeg', featured: false },
+    { id: 1, name: 'Classic White Hoodie', category: 'hoodies', image: '/assets/hoodies/hoodie1.jpeg', featured: true },
+    { id: 2, name: 'Sleeved Hoodie', category: 'hoodies', image: '/assets/hoodies/hoodie2.jpeg', featured: false },
+    { id: 3, name: 'White Pullover Hoodie', category: 'hoodies', image: '/assets/hoodies/hoodie3.jpeg', featured: false },
+    { id: 4, name: 'Pullover Hoodie', category: 'hoodies', image: '/assets/hoodies/hoodie4.jpeg', featured: false },
+    { id: 5, name: 'Orange-sleeved Pullover Hoodie', category: 'hoodies', image: '/assets/hoodies/hoodie5.jpeg', featured: false },
+    { id: 7, name: 'Gray Pullover Hoodie', category: 'hoodies', image: '/assets/hoodies/hoodie7.jpeg', featured: false },
+    { id: 8, name: 'Mint On Black Hoodie', category: 'hoodies', image: '/assets/hoodies/hoodie17.jpeg', featured: false },
+    { id: 9, name: 'Orange-sleeved Hoodie', category: 'hoodies', image: '/assets/hoodies/hoodie9.jpeg', featured: false },
+    { id: 10, name: 'Multi-designed Pullover Hoodie', category: 'hoodies', image: '/assets/hoodies/hoodie10.jpeg', featured: false },
+    { id: 11, name: 'Blue On Black Hoodie', category: 'hoodies', image: '/assets/hoodies/hoodie11.jpeg', featured: false },
+    { id: 12, name: 'Green On Black Hoodie', category: 'hoodies', image: '/assets/hoodies/hoodie16.jpeg', featured: false },
+    { id: 13, name: 'Orange On Black Hoodie', category: 'hoodies', image: '/assets/hoodies/hoodie13.jpeg', featured: false },
     
     // T-SHIRTS
-    { id: 15, name: 'White Logo Tee', category: 'tshirts', image: 'assets/tshirts/shirt1.jpeg', featured: true },
-    { id: 16, name: 'Light Pink Tee', category: 'tshirts', image: 'assets/tshirts/shirt2.jpeg', featured: false },
-    { id: 17, name: 'Omniacs White Tee', category: 'tshirts', image: 'assets/tshirts/shirt3.jpeg', featured: false },
+    { id: 15, name: 'White Logo Tee', category: 'tshirts', image: '/assets/tshirts/shirt1.jpeg', featured: true },
+    { id: 16, name: 'Light Pink Tee', category: 'tshirts', image: '/assets/tshirts/shirt2.jpeg', featured: false },
+    { id: 17, name: 'Omniacs White Tee', category: 'tshirts', image: '/assets/tshirts/shirt3.jpeg', featured: false },
     
     // BACKPACKS
-    { id: 18, name: 'Red Backpack', category: 'backpacks', image: 'assets/backpacks/bag1.jpeg', featured: true },
-    { id: 19, name: 'Blue Backpack', category: 'backpacks', image: 'assets/backpacks/bag2.jpeg', featured: false },
-    { id: 20, name: 'Omniacs Red Backpack', category: 'backpacks', image: 'assets/backpacks/bag3.jpeg', featured: false },
-    { id: 21, name: 'Yellow Backpack', category: 'backpacks', image: 'assets/backpacks/bag4.jpeg', featured: false },
-    { id: 22, name: 'Cute Pink Backpack', category: 'backpacks', image: 'assets/backpacks/bag5.jpeg', featured: false },
+    { id: 18, name: 'Red Backpack', category: 'backpacks', image: '/assets/backpacks/bag1.jpeg', featured: true },
+    { id: 19, name: 'Blue Backpack', category: 'backpacks', image: '/assets/backpacks/bag2.jpeg', featured: false },
+    { id: 20, name: 'Omniacs Red Backpack', category: 'backpacks', image: '/assets/backpacks/bag3.jpeg', featured: false },
+    { id: 21, name: 'Yellow Backpack', category: 'backpacks', image: '/assets/backpacks/bag4.jpeg', featured: false },
+    { id: 22, name: 'Cute Pink Backpack', category: 'backpacks', image: '/assets/backpacks/bag5.jpeg', featured: false },
     
     // BAGS
-    { id: 23, name: 'Blue Duffel Bag', category: 'bags', image: 'assets/bags/bag6.jpeg', featured: false },
-    { id: 24, name: 'Orange Duffel Bag', category: 'bags', image: 'assets/bags/bag7.jpeg', featured: false },
+    { id: 23, name: 'Blue Duffel Bag', category: 'bags', image: '/assets/bags/bag6.jpeg', featured: false },
+    { id: 24, name: 'Orange Duffel Bag', category: 'bags', image: '/assets/bags/bag7.jpeg', featured: false },
     
     // SPECIAL BAGS - CAROUSEL ITEM (Multiple images for one product)
     { 
         id: 25, 
         name: 'Tote Bag Collection', 
         category: 'special-bags', 
-        image: 'assets/special-bag/totebag1.jpeg',
+        image: '/assets/special-bag/totebag1.jpeg',
         images: [
-            'assets/special-bag/totebag1.jpeg',
-            'assets/special-bag/totebag2.jpeg',
-            'assets/special-bag/totebag3.jpeg'
+            '/assets/special-bag/totebag1.jpeg',
+            '/assets/special-bag/totebag2.jpeg',
+            '/assets/special-bag/totebag3.jpeg'
         ],
         hasCarousel: true,
         featured: true 
@@ -50,11 +50,11 @@ const products = [
         id: 28, 
         name: 'Bucket Hat Collection', 
         category: 'bucket-hats', 
-        image: 'assets/bucket-hats/hat1.jpeg',
+        image: '/assets/bucket-hats/hat1.jpeg',
         images: [
-            'assets/bucket-hats/hat1.jpeg',
-            'assets/bucket-hats/hat2.jpeg',
-            'assets/bucket-hats/hat3.jpeg'
+            '/assets/bucket-hats/hat1.jpeg',
+            '/assets/bucket-hats/hat2.jpeg',
+            '/assets/bucket-hats/hat3.jpeg'
         ],
         hasCarousel: true,
         featured: true 
@@ -64,84 +64,84 @@ const products = [
     { id: 31, 
       name: 'Gaming Cards', 
       category: 'gifts', 
-      image: 'assets/gifts/cards1.png', 
+      image: '/assets/gifts/cards1.png', 
       images: [
-            'assets/gifts/cards.png',
-            'assets/gifts/cards2.png',
-            'assets/gifts/cards3.png',
-            'assets/gifts/cards4.png',
-            'assets/gifts/cards5.png',
-            'assets/gifts/cards6.png',
-            'assets/gifts/cards7.png',
-            'assets/gifts/cards8.png',
-            'assets/gifts/cards9.png',
-            'assets/gifts/cards10.png',
+            '/assets/gifts/cards.png',
+            '/assets/gifts/cards2.png',
+            '/assets/gifts/cards3.png',
+            '/assets/gifts/cards4.png',
+            '/assets/gifts/cards5.png',
+            '/assets/gifts/cards6.png',
+            '/assets/gifts/cards7.png',
+            '/assets/gifts/cards8.png',
+            '/assets/gifts/cards9.png',
+            '/assets/gifts/cards10.png',
 
         ],
       hasCarousel: true,
 
       featured: false 
     },
-    { id: 32, name: 'Baby Puppets', category: 'gifts', image: 'assets/gifts/puppet.jpeg', featured: false },
+    { id: 32, name: 'Baby Puppets', category: 'gifts', image: '/assets/gifts/puppet.jpeg', featured: false },
     
     // SOCKS
-    { id: 33, name: 'Patterned Socks', category: 'socks', image: 'assets/socks/sock1.jpeg', featured: false },
-    { id: 34, name: 'Anime Socks', category: 'socks', image: 'assets/socks/sock2.jpeg', featured: false },
-    { id: 35, name: 'Comfort Socks', category: 'socks', image: 'assets/socks/sock4.jpeg', featured: false },
+    { id: 33, name: 'Patterned Socks', category: 'socks', image: '/assets/socks/sock1.jpeg', featured: false },
+    { id: 34, name: 'Anime Socks', category: 'socks', image: '/assets/socks/sock2.jpeg', featured: false },
+    { id: 35, name: 'Comfort Socks', category: 'socks', image: '/assets/socks/sock4.jpeg', featured: false },
    
     // ONESIES
-    { id: 36, name: 'Cozy Onesie', category: 'onesies', image: 'assets/onesies/onesies3.jpeg', featured: false },
+    { id: 36, name: 'Cozy Onesie', category: 'onesies', image: '/assets/onesies/onesies3.jpeg', featured: false },
     
     // TROUSERS
-    { id: 37, name: 'Gray SweatPants', category: 'trousers', image: 'assets/trousers/pant4.jpeg', featured: false },
-    { id: 38, name: 'Brown Sweatpants', category: 'trousers', image: 'assets/trousers/pant5.jpeg', featured: true },
-    { id: 39, name: 'Beige Pants', category: 'trousers', image: 'assets/trousers/pant6.jpeg', featured: false },
-    { id: 40, name: 'Yellow Pants', category: 'trousers', image: 'assets/trousers/pant7.jpeg', featured: false },
-    { id: 41, name: 'Dull-blue SweatPants', category: 'trousers', image: 'assets/trousers/pant8.jpeg', featured: false },
+    { id: 37, name: 'Gray SweatPants', category: 'trousers', image: '/assets/trousers/pant4.jpeg', featured: false },
+    { id: 38, name: 'Brown Sweatpants', category: 'trousers', image: '/assets/trousers/pant5.jpeg', featured: true },
+    { id: 39, name: 'Beige Pants', category: 'trousers', image: '/assets/trousers/pant6.jpeg', featured: false },
+    { id: 40, name: 'Yellow Pants', category: 'trousers', image: '/assets/trousers/pant7.jpeg', featured: false },
+    { id: 41, name: 'Dull-blue SweatPants', category: 'trousers', image: '/assets/trousers/pant8.jpeg', featured: false },
     
     // HOME - DRINKWARE (RENAMED to avoid duplicates)
-    { id: 42, name: 'Omniac Logo Mug Set', category: 'home', image: 'assets/home/drinkware.jpeg', featured: false },
-    { id: 43, name: 'Omniac Purple Drinkware', category: 'home', image: 'assets/home/drinkware1.jpeg', featured: false },
-    { id: 44, name: 'Omniac Drink Mugs', category: 'home', image: 'assets/home/drinkware2.jpeg', featured: false },
-    { id: 45, name: 'Omniac Glass Set', category: 'home', image: 'assets/home/drinkware3.jpeg', featured: false },
-    { id: 46, name: 'Omniac Travel Mug', category: 'home', image: 'assets/home/drinkware7.jpeg', featured: false },
-    { id: 47, name: 'Omniac Coffee Cup Collection', category: 'home', image: 'assets/home/drinkware9.jpeg', featured: false },
+    { id: 42, name: 'Omniac Logo Mug Set', category: 'home', image: '/assets/home/drinkware.jpeg', featured: false },
+    { id: 43, name: 'Omniac Purple Drinkware', category: 'home', image: '/assets/home/drinkware1.jpeg', featured: false },
+    { id: 44, name: 'Omniac Drink Mugs', category: 'home', image: '/assets/home/drinkware2.jpeg', featured: false },
+    { id: 45, name: 'Omniac Glass Set', category: 'home', image: '/assets/home/drinkware3.jpeg', featured: false },
+    { id: 46, name: 'Omniac Travel Mug', category: 'home', image: '/assets/home/drinkware7.jpeg', featured: false },
+    { id: 47, name: 'Omniac Coffee Cup Collection', category: 'home', image: '/assets/home/drinkware9.jpeg', featured: false },
     
     // TEAPOTS - CAROUSEL ITEM (Multiple images for one product)
     { 
         id: 48, 
         name: 'Omniac Teapot Collection', 
         category: 'home', 
-        image: 'assets/home/teapot1.jpeg',
+        image: '/assets/home/teapot1.jpeg',
         images: [
-            'assets/home/teapot1.jpeg',
-            'assets/home/teapot4.jpeg'
+            '/assets/home/teapot1.jpeg',
+            '/assets/home/teapot4.jpeg'
         ],
         hasCarousel: true,
         featured: true 
     },
    
     // NECKLACES (RENAMED to avoid duplicates)
-    { id: 50, name: 'Silver Chain Necklace', category: 'necklaces', image: 'assets/necklaces/necklace1.jpeg', featured: true },
-    { id: 51, name: 'Gold Chain Necklace', category: 'necklaces', image: 'assets/necklaces/necklace2.jpeg', featured: false },
-    { id: 52, name: 'Rose Gold Chain Necklace', category: 'necklaces', image: 'assets/necklaces/necklase3.jpeg', featured: false },
-    { id: 53, name: 'Platinum Chain Necklace', category: 'necklaces', image: 'assets/necklaces/necklase4.jpeg', featured: false },
-    { id: 54, name: 'Bronze Chain Necklace', category: 'necklaces', image: 'assets/necklaces/necklase5.jpeg', featured: false },
+    { id: 50, name: 'Silver Chain Necklace', category: 'necklaces', image: '/assets/necklaces/necklace1.jpeg', featured: true },
+    { id: 51, name: 'Gold Chain Necklace', category: 'necklaces', image: '/assets/necklaces/necklace2.jpeg', featured: false },
+    { id: 52, name: 'Rose Gold Chain Necklace', category: 'necklaces', image: '/assets/necklaces/necklase3.jpeg', featured: false },
+    { id: 53, name: 'Platinum Chain Necklace', category: 'necklaces', image: '/assets/necklaces/necklase4.jpeg', featured: false },
+    { id: 54, name: 'Bronze Chain Necklace', category: 'necklaces', image: '/assets/necklaces/necklase5.jpeg', featured: false },
     
     // OFFICE
-    { id: 55, name: 'Binder', category: 'office', image: 'assets/office/binder.jpeg', featured: false },
-    { id: 56, name: 'Envelope', category: 'office', image: 'assets/office/envelope3.jpeg', featured: false },
-    { id: 57, name: 'Marker', category: 'office', image: 'assets/office/markers2.jpeg', featured: false },
-    { id: 58, name: 'Mousepad', category: 'office', image: 'assets/office/mousepad.jpeg', featured: false },
-    { id: 59, name: 'Paperweights', category: 'office', image: 'assets/office/paperweights2.jpeg', featured: false },
-    { id: 60, name: 'Paper Puncher', category: 'office', image: 'assets/office/puncher1.jpeg', featured: false },
-    { id: 61, name: 'Scissors', category: 'office', image: 'assets/office/scissors.jpeg', featured: false },
-    { id: 62, name: 'Sharpener', category: 'office', image: 'assets/office/sharpener.jpeg', featured: false },
-    { id: 63, name: 'Stamper', category: 'office', image: 'assets/office/stamper1.jpeg', featured: false },
-    { id: 64, name: 'Stapler', category: 'office', image: 'assets/office/stapler2.jpeg', featured: false },
-    { id: 65, name: 'Sticky Notes', category: 'office', image: 'assets/office/stickynotes.jpeg', featured: false },
-    { id: 66, name: 'Tape Dispenser', category: 'office', image: 'assets/office/tapedispenser1.jpeg', featured: false },
-    { id: 67, name: 'Trasher', category: 'office', image: 'assets/office/papertrasher.jpeg', featured: false }
+    { id: 55, name: 'Binder', category: 'office', image: '/assets/office/binder.jpeg', featured: false },
+    { id: 56, name: 'Envelope', category: 'office', image: '/assets/office/envelope3.jpeg', featured: false },
+    { id: 57, name: 'Marker', category: 'office', image: '/assets/office/markers2.jpeg', featured: false },
+    { id: 58, name: 'Mousepad', category: 'office', image: '/assets/office/mousepad.jpeg', featured: false },
+    { id: 59, name: 'Paperweights', category: 'office', image: '/assets/office/paperweights2.jpeg', featured: false },
+    { id: 60, name: 'Paper Puncher', category: 'office', image: '/assets/office/puncher1.jpeg', featured: false },
+    { id: 61, name: 'Scissors', category: 'office', image: '/assets/office/scissors.jpeg', featured: false },
+    { id: 62, name: 'Sharpener', category: 'office', image: '/assets/office/sharpener.jpeg', featured: false },
+    { id: 63, name: 'Stamper', category: 'office', image: '/assets/office/stamper1.jpeg', featured: false },
+    { id: 64, name: 'Stapler', category: 'office', image: '/assets/office/stapler2.jpeg', featured: false },
+    { id: 65, name: 'Sticky Notes', category: 'office', image: '/assets/office/stickynotes.jpeg', featured: false },
+    { id: 66, name: 'Tape Dispenser', category: 'office', image: '/assets/office/tapedispenser1.jpeg', featured: false },
+    { id: 67, name: 'Trasher', category: 'office', image: '/assets/office/papertrasher.jpeg', featured: false }
 ];
 
 // ==================== CART MANAGEMENT ====================
